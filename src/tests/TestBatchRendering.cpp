12d231f048ef0f75d4bfff08e7192c16e4e1d10b
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2022-12-25 23:32:08 +0000

    Try out perspective camera

--- src/tests/TestBatchRendering.cpp
+++ src/tests/TestBatchRendering.cpp
@@ -13,8 +13,8 @@
 namespace test {
 
     TestBatchRendering::TestBatchRendering()
-        : translation(200, 200, 0),
-        cameraTranslation(0, 0, 0), scale(0.5), rotation(90.0f) {
+        : translation(0, 0, 0),
+        cameraTranslation(0, 0, 0), scale(1.0), rotation(45.0f) {
 
         // TODO: pass screen coords in instead of creating these raw pointers here
         // to get screen measurements
@@ -30,16 +30,28 @@ namespace test {
         // We render 2 quads using batch rendering - all vertices defined in vbo
         // 1,2 - positions, 3,4 - tex coords, 5,6,7,8 - color
         float vertices [] = {
-            100.0f, 100.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-            600.0, 100.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-            600.0f, 400.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-            100.0f, 400.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+            0.1f, 0.1f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+            0.6f, 0.1f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+            0.6f, 0.4f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+            1.0f, 0.4f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
 
-            600.0f, 100.0f, 0.0f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f,
-            1100.0, 100.0f, 1.0f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f,
-            1100.0f, 400.0f, 1.0f, 1.0f, 1.0f, 0.93f, 0.24f, 1.0f,
-            600.0f, 400.0f, 0.0f, 1.0f, 1.0f, 0.93f, 0.24f, 1.0f
+            0.6f, 0.1f, 0.0f, 0.0f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f,
+            1.1f, 0.1f, 0.0f, 1.0f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f,
+            1.1f, 0.4f, 0.0f, 1.0f, 1.0f, 1.0f, 0.93f, 0.24f, 1.0f,
+            0.6f, 0.4f, 0.0f, 0.0f, 1.0f, 1.0f, 0.93f, 0.24f, 1.0f
         };
+
+        //float vertices [] = {
+            //100.0f, 100.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+            //600.0, 100.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+            //600.0f, 400.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+            //100.0f, 400.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+
+            //600.0f, 100.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f,
+            //1100.0, 100.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f,
+            //1100.0f, 400.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.93f, 0.24f, 1.0f,
+            //600.0f, 400.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.93f, 0.24f, 1.0f
+        //};
 
         // Index vertices to save memory, only need to define 4 vertices instead of 6 to draw square
         unsigned int indices[] = {
@@ -61,10 +73,10 @@ namespace test {
         // also let's us specify different vertex layouts, default vao can be used with compability profile
         // core profile requires vao to be set
         vao = std::make_unique<VertexArray>();
-        vbo = std::make_unique<VertexBuffer>(vertices, 8 * 8 * sizeof(float));
+        vbo = std::make_unique<VertexBuffer>(vertices, 8 * 9 * sizeof(float));
 
         VertexBufferLayout layout;
-        layout.push<float>(2); // position
+        layout.push<float>(3); // position
         layout.push<float>(2); // texture coords
         layout.push<float>(4); // vertex color
 
@@ -72,17 +84,6 @@ namespace test {
 
         // Generate and bind index buffer object
         ibo = std::make_unique<IndexBuffer>(indices, 12);
-
-        // Create 4:3 aspect ratio projection ortographic matrix
-        // This helps us use easier coords system, and it gets converted back to GL system in vertex shader, by multypling
-        // each vertex position by this matrix
-        proj = glm::ortho(0.0f, (float)screenWidth, 0.0f, (float)screenHeight, -1.0f, 1.0f);
-
-        // Demonstration of how our vertex position goes back to gl coords system by multiplying
-        glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
-        glm::vec4 result = proj * vp;
-        std::cout << "Vertex position as defined by us: "<< glm::to_string(vp) << std::endl;
-        std::cout << "Vertex position after being multiplied with projection matrix: "<< glm::to_string(result) << std::endl;
 
         shader = std::make_unique<Shader>("assets/shaders/batch.glsl");
         shader->bind();
@@ -108,11 +109,13 @@ namespace test {
 
         texture->bind(); // bound to default slot 0
 
+        //proj = glm::ortho(0.0f, (float)screenWidth, 0.0f, (float)screenHeight, -1.0f, 1.0f);
+        proj = glm::perspective(glm::radians(rotation), (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
+
         // Creating view matrix, first param here is identity view matrix
         glm::mat4 view = glm::translate(glm::mat4(1.0f), cameraTranslation);
         // Creating model matrix for transformations
         glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0, 0.0, 1.0));
         model = glm::scale(model, glm::vec3(scale, scale, scale));
 
         // MVP gets multiplied in reverse order here, because OpenGL stores matrices in column order
@@ -129,15 +132,16 @@ namespace test {
 
     void TestBatchRendering::onImGuiRender() {
         ImGui::Text("Model Matrix");
-        ImGui::SliderFloat("Translation X", &translation.x, 0.0f, screenWidth);
-        ImGui::SliderFloat("Translation Y", &translation.y, 0.0f, screenHeight);
-        ImGui::SliderFloat("Translation Z", &translation.z, -1.0f, 1.0f);
+        //ImGui::SliderFloat("Translation X", &translation.x, 0.0f, screenWidth);
+        //ImGui::SliderFloat("Translation Y", &translation.y, 0.0f, screenHeight);
+        //ImGui::SliderFloat("Translation Z", &translation.z, -1.0f, 1.0f);
         ImGui::SliderFloat("Scale", &scale, 0.0f, 5.0f);
-        ImGui::SliderFloat("Rotation", &rotation, -180.0f, 180.0f);
         ImGui::Text("View Matrix");
-        ImGui::SliderFloat("View Translation X", &cameraTranslation.x, 0.0f, screenWidth);
-        ImGui::SliderFloat("View Translation Y", &cameraTranslation.y, 0.0f, screenHeight);
+        ImGui::SliderFloat("View Translation X", &cameraTranslation.x, -1.0f, 1.0f);
+        ImGui::SliderFloat("View Translation Y", &cameraTranslation.y, -1.0f, 1.0f);
         ImGui::SliderFloat("View Translation Z", &cameraTranslation.z, -1.0f, 1.0f);
+        ImGui::Text("Projection Matrix");
+        ImGui::SliderFloat("Rotation", &rotation, -180.0f, 180.0f);
 
     }
 }
